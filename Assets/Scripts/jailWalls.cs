3faad884f672b293666c6099c149ffe89a031d42
Gal Shiff <galshiff@gmail.com>
2022-03-04 17:19:17 +0200

    [Fix] Fix jail walls prefab and update game ball

--- Assets/Scripts/jailWalls.cs
+++ Assets/Scripts/jailWalls.cs
@@ -8,6 +8,8 @@ public class jailWalls : MonoBehaviour
     public GameObject west_brick;
     public GameObject west_ball;
 
+    public bool activated = false;
+
     public ParticleSystem brickExplosionParticle;
 
 
@@ -22,17 +24,35 @@ public class jailWalls : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.G))
         {
-            // Remove the brick
-            west_brick.SetActive(false);
-
-            // Activate the explosion partical system event in the brick's location
-            brickExplosionParticle.gameObject.SetActive(true);
-            brickExplosionParticle.gameObject.transform.position = west_brick.transform.position;
-            brickExplosionParticle.Play();
-
-            // TODO: Replace ball to special ball
-            // Reveal the ball
-            west_ball.SetActive(true);
+            RevealBallInsideWall();
         }
+
+        if (gameObject.activeInHierarchy && !activated)
+        {
+            activated = true;
+            // RevealBallInsideWall();
+            StartCoroutine(SetTimeout(5f));
+        }
+    }
+
+    void RevealBallInsideWall()
+    {
+        // Remove the brick
+        west_brick.SetActive(false);
+
+        // Activate the explosion partical system event in the brick's location
+        brickExplosionParticle.gameObject.SetActive(true);
+        brickExplosionParticle.gameObject.transform.position = west_brick.transform.position;
+        brickExplosionParticle.Play();
+
+        // TODO: Replace ball to special ball
+        // Reveal the ball
+        west_ball.SetActive(true);
+    }
+
+    IEnumerator SetTimeout(float time)
+    {
+        yield return new WaitForSeconds(time);
+        RevealBallInsideWall();
     }
 }
