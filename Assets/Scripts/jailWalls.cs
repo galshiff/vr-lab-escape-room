d27a23a4d19ef4dd2ecceea214a4eba99ae9ab39
Gal Shiff <galshiff@gmail.com>
2022-03-04 19:29:52 +0200

    [Fix] Freeze walls position after falling

--- Assets/Scripts/jailWalls.cs
+++ Assets/Scripts/jailWalls.cs
@@ -8,7 +8,8 @@ public class jailWalls : MonoBehaviour
     public GameObject west_brick;
     public GameObject west_ball;
 
-    public bool activated = false;
+    private bool activated = false;
+    private int WALL_NUM = 4;
 
     public ParticleSystem brickExplosionParticle;
 
@@ -30,7 +31,6 @@ public class jailWalls : MonoBehaviour
         if (gameObject.activeInHierarchy && !activated)
         {
             activated = true;
-            // RevealBallInsideWall();
             StartCoroutine(SetTimeout(5f));
         }
     }
@@ -46,13 +46,21 @@ public class jailWalls : MonoBehaviour
         brickExplosionParticle.Play();
 
         // TODO: Replace ball to special ball
-        // Reveal the ball
+        // Reveal the 
         west_ball.SetActive(true);
     }
 
     IEnumerator SetTimeout(float time)
     {
         yield return new WaitForSeconds(time);
+        // Freeze walls position
+        for (int i = 0; i < WALL_NUM; i++)
+        {
+            GameObject wall = gameObject.transform.GetChild(i).gameObject;
+            wall.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+            wall.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+        }
         RevealBallInsideWall();
     }
 }
