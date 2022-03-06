2a56b553a438226b5c1e44ff557a44ca57ae0165
Gal Shiff <galshiff@gmail.com>
2022-03-06 22:36:44 +0200

    Add scripts for keyboard integrations

---
+++ Assets/Button/Scripts/Keyboard.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Keyboard : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public GameObject normalButtons;
+    public GameObject capsButtons;
+    private bool caps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        caps = false;
+    }
+
+    public void InsertChar(string c)
+    {
+        inputField.text += caps;
+    }
+
+    public void DeleteChar()
+    {
+        if (inputField.text.Length > 0)
+        {
+            inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
+        }
+    }
+
+    public void InsertSpace()
+    {
+        inputField.text = " ";
+    }
+
+    public void CapsPerssed()
+    {
+        if (!caps)
+        {
+            normalButtons.SetActive(false);
+            capsButtons.SetActive(true);
+            caps = true;
+        }
+        else
+        {
+            normalButtons.SetActive(true);
+            capsButtons.SetActive(false);
+            caps = false;
+        }
+    }
+
+
+}
---
+++ Assets/Button/Scripts/KeyboardButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class KeyboardButton : MonoBehaviour
+{
+    Keyboard keyboard;
+    TextMeshProUGUI buttonTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyboard = GetComponent<Keyboard>();
+        buttonTxt = GetComponent<TextMeshProUGUI>();
+        if (buttonTxt.text.Length == 1)
+        {
+            NameToButtonTxt();
+            GetComponentInChildren<ButtonVR>().onRelease.AddListener(delegate { 
+                keyboard.InsertChar(buttonTxt.text);
+            });
+        }
+    }
+
+    private void NameToButtonTxt()
+    {
+        buttonTxt.text = gameObject.name;
+    }
+}
